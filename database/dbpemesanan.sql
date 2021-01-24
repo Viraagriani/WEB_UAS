ea5a7fb509e6ef8098bb3384861c43de6fbc1ab9
viraagriani <viraagriani03@gmail.com>
2021-01-24 19:01:55 +0800

    UAS WEB

---
+++ database/dbpemesanan.sql
@@ -0,0 +1,217 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 22 Jan 2021 pada 11.30
+-- Versi server: 10.4.14-MariaDB
+-- Versi PHP: 7.2.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbpemesanan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pemesanan`
+--
+
+CREATE TABLE `pemesanan` (
+  `id_pemesanan` int(50) NOT NULL,
+  `tanggal_pemesanan` date NOT NULL,
+  `total_belanja` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `pemesanan`
+--
+
+INSERT INTO `pemesanan` (`id_pemesanan`, `tanggal_pemesanan`, `total_belanja`) VALUES
+(35, '2020-06-22', 50000),
+(38, '2020-06-22', 17000),
+(39, '2020-06-22', 25000),
+(40, '2020-06-22', 22000),
+(41, '2020-06-22', 56000);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pemesanan_produk`
+--
+
+CREATE TABLE `pemesanan_produk` (
+  `id_pemesanan_produk` int(50) NOT NULL,
+  `id_pemesanan` int(50) NOT NULL,
+  `id_menu` varchar(50) NOT NULL,
+  `jumlah` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `pemesanan_produk`
+--
+
+INSERT INTO `pemesanan_produk` (`id_pemesanan_produk`, `id_pemesanan`, `id_menu`, `jumlah`) VALUES
+(7, 32, '9', 1),
+(8, 32, '11', 1),
+(9, 33, '16', 1),
+(10, 33, '6', 1),
+(11, 34, '13', 1),
+(12, 34, '8', 1),
+(13, 34, '9', 1),
+(14, 34, '17', 1),
+(15, 35, '9', 2),
+(16, 35, '14', 1),
+(17, 36, '8', 1),
+(18, 37, '13', 1),
+(19, 37, '16', 1),
+(20, 38, '8', 1),
+(21, 39, '9', 1),
+(22, 39, '16', 1),
+(23, 40, '10', 1),
+(24, 40, '14', 1),
+(25, 41, '17', 1),
+(26, 41, '10', 1),
+(27, 41, '9', 2),
+(28, 42, '9', 1),
+(29, 42, '14', 1),
+(30, 42, '7', 1),
+(31, 42, '17', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `produk`
+--
+
+CREATE TABLE `produk` (
+  `id_menu` int(50) NOT NULL,
+  `nama_menu` varchar(50) NOT NULL,
+  `jenis_menu` varchar(50) NOT NULL,
+  `stok` int(50) NOT NULL,
+  `harga` int(50) NOT NULL,
+  `gambar` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `produk`
+--
+
+INSERT INTO `produk` (`id_menu`, `nama_menu`, `jenis_menu`, `stok`, `harga`, `gambar`) VALUES
+(6, 'Bakso Urat', 'Makanan', 80, 12000, 'bakso.jpeg'),
+(7, 'Mie Ayam', 'Makanan', 50, 13000, 'mieayam.jpg'),
+(8, 'Mie Ayam Bakso', 'Makanan', 50, 17000, 'mieayambakso.jpg'),
+(9, 'Ayam Bakar', 'Makanan', 45, 20000, 'ayambakar.jpg'),
+(10, 'Lele Bakar', 'Makanan', 50, 12000, 'lele.jpg'),
+(11, 'Nasi Goreng', 'Makanan', 78, 10000, 'nasgor.jpg'),
+(12, 'Nasi Putih', 'Makanan', 100, 2000, 'nasi.jpeg'),
+(13, 'Es Jeruk', 'Makanan', 55, 8000, 'esjeruk.jpg'),
+(14, 'Jus Alpukat', 'Minuman', 50, 10000, 'juspukat.jpg'),
+(15, 'Jus Mangga', 'Minuman', 50, 10000, 'jusmangga.jpg'),
+(16, 'Es Teh', '', 60, 5000, 'tehobeng.jpg'),
+(17, 'Air Mineral', 'Minuman', 100, 4000, 'sanford.jpg'),
+(21, 'Ayam Bakar', '', 5, 15000, 'Ayam-Bakar.jpg'),
+(22, 'Nasi Uduk', 'Makanan', 2, 12000, 'Nasi-Uduk.jpg'),
+(23, 'Ayam Serundeng', 'Makanan', 7, 17000, 'Ayam-Serundeng.jpg'),
+(24, 'Ayam Geprek', 'Makanan', 4, 20000, 'Ayam-Geprek.jpg'),
+(25, 'Jahe Panas', 'Minuman', 4, 7000, 'Jahe-Panas.jpg'),
+(26, 'Jus Tomat', 'Minuman', 4, 5000, 'Jus-Tomat.jpg'),
+(27, 'Nasi Goreng', '', 5, 14000, 'Nasi-Goreng.jpg'),
+(28, 'Sup ', 'Makanan', 8, 14000, 'Sup-Buntut.jpg'),
+(29, 'Nasi Kuning', '', 9, 16000, 'Nasi-Kuning.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(25) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(25) NOT NULL,
+  `nama_lengkap` varchar(25) NOT NULL,
+  `jenis_kelamin` varchar(25) NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `alamat` varchar(25) NOT NULL,
+  `hp` varchar(25) NOT NULL,
+  `status` enum('admin','user','','') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id_user`, `username`, `password`, `nama_lengkap`, `jenis_kelamin`, `tanggal_lahir`, `alamat`, `hp`, `status`) VALUES
+(7, 'admin', 'admin123', 'admin', 'Laki-Laki', '1995-01-01', '', '', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `pemesanan`
+--
+ALTER TABLE `pemesanan`
+  ADD PRIMARY KEY (`id_pemesanan`);
+
+--
+-- Indeks untuk tabel `pemesanan_produk`
+--
+ALTER TABLE `pemesanan_produk`
+  ADD PRIMARY KEY (`id_pemesanan_produk`);
+
+--
+-- Indeks untuk tabel `produk`
+--
+ALTER TABLE `produk`
+  ADD PRIMARY KEY (`id_menu`);
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `pemesanan`
+--
+ALTER TABLE `pemesanan`
+  MODIFY `id_pemesanan` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+
+--
+-- AUTO_INCREMENT untuk tabel `pemesanan_produk`
+--
+ALTER TABLE `pemesanan_produk`
+  MODIFY `id_pemesanan_produk` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+
+--
+-- AUTO_INCREMENT untuk tabel `produk`
+--
+ALTER TABLE `produk`
+  MODIFY `id_menu` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
